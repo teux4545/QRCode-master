6cb5a2478cf7dc9618cd5317e87d3606c5ff4f8c
Teux <ciuccioveleonardo@gmail.com>
2021-12-26 22:14:10 +0100

    Snellimento codice parti inutili

--- app/src/main/java/com/example/enamul/qrcode/MainActivity.java
+++ app/src/main/java/com/example/enamul/qrcode/MainActivity.java
@@ -1,18 +1,16 @@
 package com.example.enamul.qrcode;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.util.ArrayList;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,23 +18,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.enamul.getDati.Dati;
 import com.example.enamul.getDati.asyncTaskGetDati;
 import com.example.enamul.listStructure.listStruct;
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.MultiFormatWriter;
-import com.google.zxing.WriterException;
-import com.google.zxing.common.BitMatrix;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
-import java.util.ArrayList;
-
 public class MainActivity extends AppCompatActivity {
-    ImageView imageView;
-    Button button;
+
     Button btnScan;
-    EditText editText;
-    String EditTextValue ;
-    public final static int QRcodeWidth = 350 ;
-    Bitmap bitmap ;
 
     listStruct control = new listStruct();
     ArrayList<String> list = new ArrayList<>();
@@ -54,36 +41,9 @@ public class MainActivity extends AppCompatActivity {
 
         new asyncTaskGetDati().execute();
 
-        imageView = (ImageView)findViewById(R.id.imageView);
-        editText = (EditText)findViewById(R.id.editText);
-        button = (Button)findViewById(R.id.button);
         btnScan = (Button)findViewById(R.id.btnScan);
         tv_qr_readTxt = (TextView) findViewById(R.id.tv_qr_readTxt);
         tv_qr_readTxt2 = (TextView) findViewById(R.id.tv_qr_readTxt2);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                if(!editText.getText().toString().isEmpty()){
-                    EditTextValue = editText.getText().toString();
-
-                    try {
-                        bitmap = TextToImageEncode(EditTextValue);
-
-                        imageView.setImageBitmap(bitmap);
-
-                    } catch (WriterException e) {
-                        e.printStackTrace();
-                    }
-                }
-                else{
-                    editText.requestFocus();
-                    Toast.makeText(MainActivity.this, "Please Enter Your Scanned Test" , Toast.LENGTH_LONG).show();
-                }
-
-            }
-        });
 
 
         btnScan.setOnClickListener(new View.OnClickListener() {
@@ -101,43 +61,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-
-    Bitmap TextToImageEncode(String Value) throws WriterException {
-        BitMatrix bitMatrix;
-        try {
-            bitMatrix = new MultiFormatWriter().encode(
-                    Value,
-                    BarcodeFormat.DATA_MATRIX.QR_CODE,
-                    QRcodeWidth, QRcodeWidth, null
-            );
-
-        } catch (IllegalArgumentException Illegalargumentexception) {
-
-            return null;
-        }
-        int bitMatrixWidth = bitMatrix.getWidth();
-
-        int bitMatrixHeight = bitMatrix.getHeight();
-
-        int[] pixels = new int[bitMatrixWidth * bitMatrixHeight];
-
-        for (int y = 0; y < bitMatrixHeight; y++) {
-            int offset = y * bitMatrixWidth;
-
-            for (int x = 0; x < bitMatrixWidth; x++) {
-
-                pixels[offset + x] = bitMatrix.get(x, y) ?
-                        getResources().getColor(R.color.QRCodeBlackColor):getResources().getColor(R.color.QRCodeWhiteColor);
-            }
-        }
-        Bitmap bitmap = Bitmap.createBitmap(bitMatrixWidth, bitMatrixHeight, Bitmap.Config.ARGB_4444);
-
-        bitmap.setPixels(pixels, 0, 350, 0, 0, bitMatrixWidth, bitMatrixHeight);
-        return bitmap;
-    }
-
-
 
 
     @RequiresApi(api = Build.VERSION_CODES.N)
